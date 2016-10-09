ca12089ebb49e2f073b86e8225f2bafccfc0883b
Pavel_Beljaev <beljaev.pavel.kr@gmail.com>
2016-10-09 11:48:52 +0200

    add custom login page

--- src/main/java/com/pesahlavan/training/HomeController.java
+++ src/main/java/com/pesahlavan/training/HomeController.java
@@ -3,37 +3,45 @@ package com.pesahlavan.training;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+
+	@RequestMapping(value = "/user", method = RequestMethod.GET)
+	public String mainPage() {
+
+		return "/content/user";
+
 	}
-	
+
+	@RequestMapping(value = "/admin", method = RequestMethod.GET)
+	public String adminPage() {
+
+		return "/content/admin";
+
+	}
+
+	@RequestMapping(value = { "/", "/login" }, method = RequestMethod.GET)
+	public ModelAndView login(@RequestParam(value = "error", required = false) String error) {
+
+		ModelAndView model = new ModelAndView();
+		if (error != null) {
+			model.addObject("error", "Invalid username or password!");
+		}
+
+		model.setViewName("login");
+
+		return model;
+
+	}
+
 }
